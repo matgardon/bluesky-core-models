37785052c7bf068d0c3a5e52a66d6ce52f90b319
Mathis Gardon <mathis.gardon@gmail.com>
2016-09-09 14:46:43 +0200

    Added core models for auth & client config contract

---
+++ src/auth/ajax-client-endpoint-configuration.dto.ts
@@ -0,0 +1,8 @@
+﻿namespace bluesky.core.models.auth {
+	export interface AjaxClientEndpointConfigurationDto {
+		endpointBaseURL: string;
+		endpointSuffix: string;
+		authToken: string;
+		authTokenValidityEndDate: Date;
+	}
+}
---
+++ src/auth/bluesky-ajax-client-configuration.dto.ts
@@ -0,0 +1,15 @@
+﻿namespace bluesky.core.models.auth {
+	export interface BlueskyAjaxClientConfigurationDto {
+		coreApiConfiguration: AjaxClientEndpointConfigurationDto;
+		marketingApiConfiguration: AjaxClientEndpointConfigurationDto;
+		selfcareApiConfiguration: AjaxClientEndpointConfigurationDto;
+		quoteWizardConfiguration: AjaxClientEndpointConfigurationDto;
+		orderEntryConfiguration: AjaxClientEndpointConfigurationDto;
+		orderTrackingConfiguration: AjaxClientEndpointConfigurationDto;
+		technicalInventoryConfiguration: AjaxClientEndpointConfigurationDto;
+		metranetConfiguration: AjaxClientEndpointConfigurationDto;
+		salesforceConfiguration: AjaxClientEndpointConfigurationDto;
+		templateGeneratorConfiguration: AjaxClientEndpointConfigurationDto;
+		defaultUserRole: string;
+	}
+}
