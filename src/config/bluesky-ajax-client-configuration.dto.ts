8e5ec3c2c5285ae2107ffe9919a7df937811d071
Mathis Gardon <mathis.gardon@gmail.com>
2016-09-26 16:45:23 +0200

    Removed unecessary interface to describe EndpointConfigDictionnary

--- src/config/bluesky-ajax-client-configuration.dto.ts
+++ src/config/bluesky-ajax-client-configuration.dto.ts
@@ -1,20 +1,15 @@
 ﻿namespace bluesky.core.models.clientConfig {
     import UserSsoDto = models.userManagement.IUserSsoDto;
 
-    export interface IAjaxClientConfigurationDictionnary {
-        /**
-         * Dictionnary of client configuration per-endpoint.
-         * @param endpointType : of EndpoinTypeEnum type, but it cannot be strongly typed to the related enum (EndpointTypeEnum) due too TS not implementing it atm: https://github.com/Microsoft/TypeScript/issues/2491. So we use the string representation of the enum as key.
-         * @returns {} 
-         */
-        [endpointType: string]: clientConfig.IAjaxClientEndpointConfigurationDto;
-    }
-
     export interface IBlueskyAjaxClientConfigurationDto {
         /**
+         * Dictionnary of client configuration per-endpoint.
+         * @param endpointType : EndpoinTypeEnum type string representation. It cannot be strongly typed to the related enum (EndpointTypeEnum) due to TS not implementing it atm: https://github.com/Microsoft/TypeScript/issues/2491. So we use the string representation of the enum as key.
+         * @returns {}
+         *
          * TODO MGA: C# json native json serializer doesn't handle dictionnaries with enum keys, so we use an array of endpoint config based on string representation. http://stackoverflow.com/questions/2892910/problems-with-json-serialize-dictionaryenum-int32
          */
-        EndpointConfigurationDictionnary: IAjaxClientConfigurationDictionnary;
+        EndpointConfigurationDictionnary: { [endpointType: string]: clientConfig.IAjaxClientEndpointConfigurationDto; };
 
         CurrentUserRole: string;
 
