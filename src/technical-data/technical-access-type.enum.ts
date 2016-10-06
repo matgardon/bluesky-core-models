8060741e12e504c6110a56f0c847fb0b0632f187
Mathis Gardon <mathis.gardon@gmail.com>
2016-10-06 15:47:41 +0200

    added technical access type enum

---
+++ src/technical-data/technical-access-type.enum.ts
@@ -0,0 +1,9 @@
+﻿namespace bluesky.core.models.technicalData {
+    export enum TechnicalAccessTypeEnum {
+        None,
+        AudioAccess,
+        WebAccess,
+        IntegratedAudioAccess,
+        GenericAccess,
+    }
+}
