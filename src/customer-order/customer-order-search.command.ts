f22e73f311abed40cfde4fcc37dcf758b5582847
gduprez <g.duprez@arkadin.com>
2017-06-05 14:53:30 +0200

    rename orderType and set all nullable

--- src/customer-order/customer-order-search.command.ts
+++ src/customer-order/customer-order-search.command.ts
@@ -1,13 +1,13 @@
 ﻿namespace bluesky.core.command.customerOrder {
     export interface ICustomerOrderSearchCommand extends IBasePaginatedSearchCommand {
-        CustomerOrderStatusFilter: string;
-        UserAccountUid: string;
-        LogoUid: string;
-        OrderId: string;
-        LogoName: string;
-        CallingSystem: string;
+        CustomerOrderStatusFilter?: string;
+        UserAccountUid?: string;
+        LogoUid?: string;
+        OrderId?: string;
+        LogoName?: string;
+        CallingSystem?: string;
         SubsidiaryName: string;
-        OrderType: string;
-        SubmitDate: string;
+        OrderTypeFilter?: string;
+        SubmitDate?: string;
     }
 }
