9fd283b815354751a99badd8a30f33d941cefe2b
gduprez <g.duprez@arkadin.com>
2017-06-05 14:39:08 +0200

    update logoId to logoUid and added new search criterias

--- src/customer-order/customer-order-search.command.ts
+++ src/customer-order/customer-order-search.command.ts
@@ -2,7 +2,12 @@
     export interface ICustomerOrderSearchCommand extends IBasePaginatedSearchCommand {
         CustomerOrderStatusFilter: string;
         UserAccountUid: string;
-        LogoId: string;
+        LogoUid: string;
         OrderId: string;
+        LogoName: string;
+        CallingSystem: string;
+        SubsidiaryName: string;
+        OrderType: string;
+        SubmitDate: string;
     }
 }
