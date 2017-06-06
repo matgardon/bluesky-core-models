24772a548aa4b44765d5e93067af4444441a9a49
gduprez <g.duprez@arkadin.com>
2017-06-06 17:02:31 +0200

    Properties renaming in ICustomerOrderSearchCommand

--- src/customer-order/customer-order-search.command.ts
+++ src/customer-order/customer-order-search.command.ts
@@ -1,12 +1,13 @@
 ﻿namespace bluesky.core.command.customerOrder {
     export interface ICustomerOrderSearchCommand extends IBasePaginatedSearchCommand {
         CustomerOrderStatusFilter?: string;
+        CustomerOrderTypeFilter?: string;
         UserAccountUid?: string;
         LogoUid?: string;
         OrderId?: string;
         LogoName?: string;
         CallingSystem?: string;
-        SubsidiaryName: string;
+        SubsidiaryFilter?: string;
         OrderTypeFilter?: string;
         SubmitDateFrom?: string;
         SubmitDateTo?: string;
