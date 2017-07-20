5fda27c23d373e7caecd7418db5e3e963aefb951
gduprez <g.duprez@arkadin.com>
2017-07-20 11:46:32 +0200

    added formatted result contract and refactor customer-order-search command to support formatted results

---
+++ src/common/formatted-result.command.base.ts
@@ -0,0 +1,11 @@
+﻿namespace bluesky.core.command {
+	/** Shared properties for formatted results of search commands. */
+	export interface IFormattedResultCommand {
+		/** Specifies the format expected for the output response. Can be at the moment CSV & ??? TODO MGA StringEnum */
+		Format?: SupportedResponseFormatEnum;
+	}
+
+	export enum SupportedResponseFormatEnum {
+		CSV = 'csv'
+	}
+}
