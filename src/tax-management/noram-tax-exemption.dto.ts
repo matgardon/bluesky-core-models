77c7c4edf9d25451b6e85f08ed4361348d8aa699
Mathis Gardon <matgardon38@gmail.com>
2018-03-01 16:53:32 +0100

    [116176] commented all code related to noram tax exemptions

--- src/tax-management/noram-tax-exemption.dto.ts
+++ src/tax-management/noram-tax-exemption.dto.ts
@@ -1,12 +1,13 @@
-namespace bluesky.core.model.taxManagement {
-    export interface INoramTaxExemptionDto extends IResourceBase {
-        Level: TaxExemptTypeEnum;
-        Location: number;
-        CertificateId: string;
-        StartDate: string;
-        ExpirationDate: string;
-        TaxExemptionTypes: Array<ITaxExemptionTypeDto>;
-        TaxAction: TaxActionEnum;
-        IsExpired: boolean;
-    }
-}
+//TODO MGA disabling from UI following TFS id:http://frmtptfs01:8080/tfs/Arkadin/BlueSky/_workitems#id=116176 pending full removal ?
+// namespace bluesky.core.model.taxManagement {
+//     export interface INoramTaxExemptionDto extends IResourceBase {
+//         Level: TaxExemptTypeEnum;
+//         Location: number;
+//         CertificateId: string;
+//         StartDate: string;
+//         ExpirationDate: string;
+//         TaxExemptionTypes: Array<ITaxExemptionTypeDto>;
+//         TaxAction: TaxActionEnum;
+//         IsExpired: boolean;
+//     }
+// }
--- src/tax-management/tax-exemption-type.enum.ts
+++ src/tax-management/tax-exemption-type.enum.ts
@@ -1,6 +1,7 @@
-namespace bluesky.core.model.taxManagement {
-    export enum TaxExemptTypeEnum {
-        Federal = 0,
-        State   = 1
-    }
-}
+//TODO MGA disabling from UI following TFS id:http://frmtptfs01:8080/tfs/Arkadin/BlueSky/_workitems#id=116176 pending full removal ?
+// namespace bluesky.core.model.taxManagement {
+//     export enum TaxExemptTypeEnum {
+//         Federal = 0,
+//         State   = 1
+//     }
+// }
